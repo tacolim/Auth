efa1cb7047836d96975b2a6e8f736c6c6d1a215c
Cassidy SN Avery <tacolim@gmail.com>
2017-12-12 17:11:46 -0500

    attempting to get working with Ryans code, still not working....

--- src/server.js
+++ src/server.js
@@ -1,115 +1,68 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user');
 const bcrypt = require('bcrypt');
-
-const User = require('./user.js');
+const middleWare = require('./middleware');
 
 const STATUS_USER_ERROR = 422;
-const STATUS_SERVER_ERROR = 500;
 const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: false,
+  }),
+);
+server.use(middleWare.restrictedPermissions);
 
-/* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
-const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
-  }
-};
+/* ************ Routes ***************** */
 
-// TODO: implement routes
-
-const hashPass = (req, res, next) => {
-  const { password } = req.body;
-  bcrypt
-    .hash(password, BCRYPT_COST)
-    .then((pw) => {
-      req.password = pw;
-      next();
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
-};
-
-server.post('/users', hashPass, (req, res) => {
-  const username = req.body;
-  const passwordHash = req.password;
-
-  const newUser = new User({ username, passwordHash });
-
-  newUser.save((err, savedUser) => {
-    if (err) {
-      res.status(STATUS_USER_ERROR);
-      res.json({ 'Need email & pw': err.message });
-      return;
-    }
-    res.status(200).json(savedUser);
-  });
-});
-
-const authenticate = (req, res, next) => {
-  const { username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      res.status(STATUS_USER_ERROR);
-      res.json({ 'Need both Email/PW': err.message });
-      return;
-    }
-    const hashedPW = user.passwordHash;
-    bcrypt
-      .compare(password, hashedPW)
-      .then((res) => {
-        if (!res) throw new Error();
-        req.loggedInUser = user;
-        next();
-      })
-      .catch((err) => {
-        return sendUserError('some err message', res);
-      });
-  });
-};
-
-server.post('/log-in', authenticate, (req, res) => {
+server.post('/log-in', middleWare.handleLogin, (req, res) => {
   res.json({ success: true });
 });
 
-const loggedIn = (req, res, next) => {
-  const { username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
+server.post('/users', middleWare.hashedPassword, (req, res) => {
+  const { username } = req.body;
+  const passwordHash = req.password;
+  const newUser = new User({ username, passwordHash });
+  newUser.save((err, savedUser) => {
     if (err) {
-      res.status(STATUS_USER_ERROR);
-      res.json({ 'Need both Email/PW': err.message });
+      res.status(422);
+      res.json({ 'Need both username/PW fields': err.message });
       return;
     }
-    const hashedPW = user.passwordHash;
-    bcrypt
-      .compare(password, hashedPW)
-      .then((res) => {
-        if (!res) throw new Error();
-        req.loggedInUser = user;
-        next();
-      })
-      .catch((err) => {
-        return sendUserError('some err message', res);
-      });
+    res.json(savedUser);
   });
-};
+});
+
+server.post('/logout', (req, res) => {
+  if (!req.session.username) {
+    middleWare.sendUserError('User is not logged in', res);
+    return;
+  }
+  req.session.username = null;
+  res.json(req.session);
+});
+
+server.get('/restricted/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      middleWare.sendUserError('500', res);
+      return;
+    }
+    res.json(users);
+  });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', middleWare.loggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
-  res.json(req.user);
+  res.json(req.session);
 });
 
 module.exports = { server };
